c1b0c964f1b34c269f357e223fcf1009b11b48ee
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-11-07 21:24:56 +0200

    Fixing the deployment scripts #1

--- updateKube.sh
+++ updateKube.sh
@@ -4,17 +4,19 @@ cat k8s/deployment.yaml
 
 base64 --version
 
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  apply -f ./k8s/
-echo "The build number is ${TRAVIS_BUILD_NUMBER}"
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:${TRAVIS_BUILD_NUMBER} --record
+echo "${KUBERNETES_CLUSTER_CERTIFICATE}"
+
+# echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+# /usr/local/bin/kubectl \
+#   --kubeconfig=/dev/null \
+#   --server=$KUBERNETES_SERVER \
+#   --certificate-authority=cert.crt \
+#   --token=$KUBERNETES_TOKEN \
+#   apply -f ./k8s/
+# echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+# /usr/local/bin/kubectl \
+#   --kubeconfig=/dev/null \
+#   --server=$KUBERNETES_SERVER \
+#   --certificate-authority=cert.crt \
+#   --token=$KUBERNETES_TOKEN \
+#   set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:${TRAVIS_BUILD_NUMBER} --record
