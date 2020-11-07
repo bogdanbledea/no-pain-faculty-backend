5e9f79c2b95222638b628a5c91c18e7aecbac2e4
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-11-07 21:58:24 +0200

    Remove testing the deployment

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
-echo "I will fake the deployment for now"
-# echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-# /usr/local/bin/kubectl \
-#   --kubeconfig=/dev/null \
-#   --server=$KUBERNETES_SERVER \
-#   --certificate-authority=cert.crt \
-#   --token=$KUBERNETES_TOKEN \
-#   apply -f ./k8s/
-# echo "The build number is ${TRAVIS_BUILD_NUMBER}"
-# /usr/local/bin/kubectl \
-#   --kubeconfig=/dev/null \
-#   --server=$KUBERNETES_SERVER \
-#   --certificate-authority=cert.crt \
-#   --token=$KUBERNETES_TOKEN \
-#   set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:${TRAVIS_BUILD_NUMBER} --record
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./k8s/
+echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:${TRAVIS_BUILD_NUMBER} --record
