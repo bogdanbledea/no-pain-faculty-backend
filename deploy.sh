519415e932ffaf20f457852f9c4d2f8e41c94e28
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-11-08 14:40:01 +0200

    Deployment travis try #1

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  apply -f ./k8s/
+kubectl  apply -f ./k8s/
 echo "The build number is ${TRAVIS_BUILD_NUMBER}"
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:${TRAVIS_BUILD_NUMBER} --record
+kubectl set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:${TRAVIS_BUILD_NUMBER} --record
